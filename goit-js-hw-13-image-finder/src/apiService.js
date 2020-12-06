3ccd151fb9a244a1259aae20e30f73abdbf8ebe5
Roman27011986 <>
2020-12-06 14:39:08 +0200

    йцу

---
+++ goit-js-hw-13-image-finder/src/apiService.js
@@ -0,0 +1,29 @@
+const apiKey = '19349561-1c4da8557b38d24bee91f401b'
+
+export default {
+    serchQuery: '',
+    page: 1,
+    fetchHits() {
+        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.serchQuery}&page=${this.page}&per_page=12&key=${apiKey}`
+        return fetch(url)
+            .then(res => res.json())
+            .then(({ hits }) => {
+                this.page += 1
+                return hits
+            })
+    },
+    resetPage() {
+        this.page = 1;
+    },
+    get query() {
+        return this.serchQuery;
+    },
+    set query(value) {
+        this.serchQuery = value;
+    }
+
+      
+}; 
+
+
+
---
+++ goit-js-hw-13-image-finder/src/update-hits.js
@@ -0,0 +1,8 @@
+import imagesTpl from './images.hbs'
+const listRef = document.querySelector('.gallery')
+function updateHitsMarkup(hits) {
+    const markup = imagesTpl(hits)
+    listRef.insertAdjacentHTML('beforeend', markup)
+      
+}
+export default updateHitsMarkup
